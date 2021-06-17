f051757867e4e5bd7220747f4331f1ef91552869
tranngoc769 <tranngoc769@gmail.com>
2021-06-17 14:46:51 +0700

    fix url

--- application/views/suachua.php
+++ application/views/suachua.php
@@ -13,7 +13,7 @@
                            <header class="entry-header">
                               <div class="entry-header-text entry-header-text-top text-left">
                                  <h6 class="entry-category is-xsmall">
-                                    <a href="https://karofihaiduong.vn/dich-vu/sua-chua-may-loc-nuoc/" rel="category tag">Sửa chữa máy lọc nước</a>
+                                    <a href="/index.php/shop/suachua" rel="category tag">Sửa chữa máy lọc nước</a>
                                  </h6>
 
                                  <h1 class="entry-title">SỬA CHỮA MÁY LỌC NƯỚC TẠI <?= $keywords['nameUp'] ?></h1>
@@ -22,7 +22,7 @@
                               </div><!-- .entry-header -->
 
                               <div class="entry-image relative">
-                                 <a href="https://karofihaiduong.vn/sua-chua-may-loc-karofi/">
+                                 <a href="/index.php/shop/suachua">
                                     <img width="684" height="418" src="/style/uploads/2019/12/sua-may-loc-nuoc-karofi.png" class="attachment-large size-large wp-post-image" alt="" srcset="/style/uploads/2019/12/sua-may-loc-nuoc-karofi.png 684w, /style/uploads/2019/12/sua-may-loc-nuoc-karofi-655x400.png 655w, /style/uploads/2019/12/sua-may-loc-nuoc-karofi-600x367.png 600w" sizes="(max-width: 684px) 100vw, 684px"></a>
                                  <div class="badge absolute top post-date badge-square">
                                     <div class="badge-inner">
@@ -38,7 +38,7 @@
                               <p><span style="font-family: 'times new roman', times, serif; font-size: 120%; color: #000000;">Với kinh nghiệm 10 năm trong ngành lọc nước và đội ngũ kỹ thuật nhiều kinh nghiệm, Chúng tôi cam kết sửa chữa mọi loại máy lọc nước với chi phí rẻ nhất, tiết kiệm thời gian và chi phí cho khách hàng tối đa.</span></p>
                               <h3 style="text-align: center;"><span style="color: #ed1c24;"><strong><span style="font-family: 'times new roman', times, serif; font-size: 120%;">Dịch vụ sửa chữa, bảo dưỡng máy lọc nước Karofi tại <?= $keywords['nameUp'] ?></span></strong></span></h3>
                               <h4 style="text-align: center;"><span style="color: #ed1c24;"><strong>Chuyên nghiệp – Nhiệt Tình – Giá Rẻ – 24/7</strong></span></h4>
-                              <p style="text-align: center;"><span style="font-family: 'times new roman', times, serif; font-size: 120%; color: #000000;">Hotline <a href="tel:<?= $keywords['hotline'] ?>"><strong><span style="color: #ed1c24;"><?= $keywords['hotline'] ?></span></strong></a> hoặc <span style="color: #ed1c24;"><a style="color: #ed1c24;" href="tel:0937557555"><strong><?= $keywords['hotline'] ?></strong></a></span> </span></p>
+                              <p style="text-align: center;"><span style="font-family: 'times new roman', times, serif; font-size: 120%; color: #000000;">Hotline <a href="tel:<?= $keywords['hotline'] ?>"><strong><span style="color: #ed1c24;"><?= $keywords['hotline'] ?></span></strong></a> hoặc <span style="color: #ed1c24;"><a style="color: #ed1c24;" href="tel:<?= $keywords['hotline'] ?>"><strong><?= $keywords['hotline'] ?></strong></a></span> </span></p>
                               <p style="text-align: justify;"><span style="font-family: 'times new roman', times, serif; font-size: 120%; color: #000000;">Máy lọc nước là thiết bị thiết yếu được sử dụng ngày càng nhiều trong các gia đình, công sở và các khu công nghiệp. Vật liệu, lõi lọc nước là các thiết bị sử dụng có tính tiêu hao nên cần được bảo trì và thay thế định kỳ để đảm bảo chất lượng nước đầu ra. Chúng tôi cung cấp dịch vụ sửa chữa máy lọc nước tại <?= $keywords['nameUp'] ?> với chất lượng tốt linh phụ kiện chính hãng với giá cả cạnh tranh, tiết kiệm thời gian và chi phí cho khách hàng.</span></p>
                               <p style="text-align: justify;"><span style="font-size: 130%;"><strong><span style="font-family: 'times new roman', times, serif; color: #000000;"><img class="alignnone wp-image-926" src="/style/uploads/2019/12/chi-tay.png" alt="" width="45" height="23">&nbsp; Thay thế linh kiện, lõi lọc chính hãng.</span></strong></span></p>
                               <p style="text-align: justify;"><span style="font-size: 130%;"><strong><span style="font-family: 'times new roman', times, serif; color: #000000;"><img class="alignnone wp-image-926" src="/style/uploads/2019/12/chi-tay.png" alt="" width="45" height="23">&nbsp; Bảo hành 1 năm trở lên với linh kiên thay thế sửa chữa máy lọc nước.</span></strong></span></p>
@@ -58,7 +58,7 @@
                               <p style="text-align: justify;"><span style="font-family: 'times new roman', times, serif; font-size: 120%; color: #000000;">– Thay lõi lọc, lõi bẩn , đến thời hạn thay lõi lọc.</span></p>
                               <p style="text-align: justify;"><span style="font-family: 'times new roman', times, serif; font-size: 120%; color: #000000;">– Rò rỉ điện, hỏng máy bơm.</span></p>
                               <p style="text-align: justify;"><span style="font-family: 'times new roman', times, serif; font-size: 120%; color: #000000;">– Và tất cả các trường hợp khác….</span></p>
-                              <p style="text-align: justify;"><span style="font-family: 'times new roman', times, serif; font-size: 120%; color: #000000;">Ngay khi bạn liện hệ với chúng tôi qua số Hotline <a href="tel:<?= $keywords['hotline'] ?>"><strong><span style="color: #ed1c24;">077.8888.555</span></strong></a> hoặc <span style="color: #ed1c24;"><a style="color: #ed1c24;" href="tel:0937557555"><strong><?= $keywords['hotline'] ?></strong></a></span> chúng tôi luôn sẵn sàng tiếp nhận những yêu cầu của bạn, giải đáp những thắc mắc, cam kết đồng hành bằng sự tận tâm và đi đến tận cùng sức sống của sản phẩm theo đúng mong đợi của bạn.</span></p>
+                              <p style="text-align: justify;"><span style="font-family: 'times new roman', times, serif; font-size: 120%; color: #000000;">Ngay khi bạn liên hệ với chúng tôi qua số Hotline <a href="tel:<?= $keywords['hotline'] ?>"><strong><span style="color: #ed1c24;"><?= $keywords['hotline'] ?></span></strong></a> hoặc <span style="color: #ed1c24;"><a style="color: #ed1c24;" href="tel:<?= $keywords['hotline'] ?>"><strong><?= $keywords['hotline'] ?></strong></a></span> chúng tôi luôn sẵn sàng tiếp nhận những yêu cầu của bạn, giải đáp những thắc mắc, cam kết đồng hành bằng sự tận tâm và đi đến tận cùng sức sống của sản phẩm theo đúng mong đợi của bạn.</span></p>
                               <p style="text-align: justify;"><strong><span style="font-family: 'times new roman', times, serif; font-size: 120%; color: #000000;"><?= $keywords['nameUp'] ?></span></strong></p>
                               <p style="text-align: justify;"><strong><span style="font-family: 'times new roman', times, serif; font-size: 120%; color: #000000;"><?= $keywords['address'] ?></span></strong></p>
                               <p style="text-align: justify;"><strong><span style="font-family: 'times new roman', times, serif; font-size: 120%; color: #000000;">Hotline: <a href="tel:<?= $keywords['hotline'] ?>"><?= $keywords['hotline'] ?></a></span></strong></p>
@@ -68,7 +68,7 @@
                      <div id="comments" class="comments-area">
                         <div id="respond" class="comment-respond">
                            <h3 id="reply-title" class="comment-reply-title">Trả lời <small><a rel="nofollow" id="cancel-comment-reply-link" href="/sua-chua-may-loc-karofi/#respond" style="display:none;">Hủy</a></small></h3>
-                           <form action="https://karofihaiduong.vn/wp-comments-post.php" method="post" id="commentform" class="comment-form" novalidate="">
+                           <form action="#" method="post" id="commentform" class="comment-form" novalidate="">
                               <p class="comment-notes"><span id="email-notes">Email của bạn sẽ không được hiển thị công khai.</span> Các trường bắt buộc được đánh dấu <span class="required">*</span></p>
                               <p class="comment-form-comment"><label for="comment">Bình luận</label> <textarea id="comment" name="comment" cols="45" rows="8" maxlength="65525" required="required"></textarea></p>
                               <p class="comment-form-author"><label for="author">Tên <span class="required">*</span></label> <input id="author" name="author" type="text" value="" size="30" maxlength="245" required="required"></p>
